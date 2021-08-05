65499133edbedd791c11a70bae82de615780a022
Ubuntu <ubuntu@ip-172-31-48-80.ec2.internal>
2021-08-05 15:48:41 +0000

    Atualizando templates do terraform

--- terraform/firewall/main.tf
+++ terraform/firewall/main.tf
@@ -1,10 +1,27 @@
+# https://github.com/terraform-aws-modules/terraform-aws-security-group/blob/master/examples/http/main.tf
+
+
+# Configurando o cloud provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 locals {
   # Ids for multiple sets of EC2 instances, merged together
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
-data "aws_vpc" "my-vpc" {
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+data "aws_vpc" "my_vpc" {
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -15,28 +32,39 @@ module "web_server_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
   name        = "web-server"
-  description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = data.aws_vpc.my-vpc
+  description = "Security group with HTTP ports open for everybody (IPv4 CIDR), egress ports are all world open"
+  vpc_id      = data.aws_vpc.my_vpc.id
 
-  ingress_cidr_blocks = local.public_subnets
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+    Tier = "FE"
+  }
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-module "private_service_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+module "mysql_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/mysql"
 
-  name        = "private-service"
-  description = "Security group for backend services with custom ports open only for internal networks"
-  vpc_id      = data.aws_vpc.my-vpc
+  name        = "computed-mysql-sg"
+  description = "Security group with MySQL/Aurora port open for HTTP security group created above (computed)"
+  vpc_id      = data.aws_vpc.my_vpc.id
 
-  ingress_cidr_blocks      = local.private_subnets
-  ingress_rules            = ["postgresql-tcp"]
-  ingress_with_cidr_blocks = [
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+    Tier = "BE"
+  }
+
+  ingress_cidr_blocks = local.private_subnets
+
+  computed_ingress_with_source_security_group_id = [
     {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks =  local.private_subnets
-    }
+      rule                     = "mysql-tcp"
+      source_security_group_id = module.web_server_sg.security_group_id
+    },
   ]
+
+  number_of_computed_ingress_with_source_security_group_id = 1
 }
