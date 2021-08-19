f915854f3012abd06fdb27363fc7f3e22e5096f7
Helder Pereira <helcorintho@gmail.com>
2021-08-19 15:42:28 -0300

    Update main.tf

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -23,10 +23,10 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   enable_vpn_gateway = false
 
   tags = {
